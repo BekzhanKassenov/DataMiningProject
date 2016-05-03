9f63c525cb0e145fa8b21fcc5af7196e04533f10
BekzhanKassenov <bekzhan.kassenov@gmail.com>
2016-05-04 01:24:09 +0600

    Added saving precessed video into file.

--- main.cpp
+++ main.cpp
@@ -3,11 +3,17 @@
 #include <highgui.h>
 
 namespace cardet {
-    const std::string videoOutputName = "Video";
-    const std::string detectedOutputName = "After detection";
+    // Names of the windows with video
+    const std::string videoWindowName = "Video";
+    const std::string detectedWindowName = "After detection";
 
+    // Output file
+    const std::string videoOutputName = "/home/bekzhan/Programming/DataMiningProject/output.avi";
 
+    // File with input data
     std::string filename = "data/video.avi";
+
+    // File with Haar classifier data
     std::string trainedData = "trainedData/HaarFeatures.xml";
 }
 
@@ -33,6 +39,7 @@ int main(int argc, char **argv) {
 
     std::cout << "Opening " << cardet::filename << std::endl;
 
+    /* Initializing input stream */
     cv::VideoCapture input(cardet::filename);
 
     if (!input.isOpened()) {
@@ -40,8 +47,27 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    /* Opening output stream */
+    int fourcc = (int) input.get(CV_CAP_PROP_FOURCC);
+
+    double fps = input.get(CV_CAP_PROP_FPS);
+
+    cv::Size videoSize((int) input.get(CV_CAP_PROP_FRAME_WIDTH),
+                       (int) input.get(CV_CAP_PROP_FRAME_HEIGHT));
+
+    cv::VideoWriter output(cardet::videoOutputName, fourcc, fps, videoSize, true);
+
+    if (!output.isOpened()) {
+        std::cout << "Cannot open video for writing" << std::endl;
+        return 1;
+    }
+
+    // Frame to be read at each iteration
     cv::Mat frame;
+
+    // Haar Cascades Classifier
     cv::CascadeClassifier classifier(cardet::trainedData);
+
     while (true) {
         input >> frame;
 
@@ -49,8 +75,10 @@ int main(int argc, char **argv) {
             break;
         }
 
+        // List of objects that were detected by classifier
         std::vector <cv::Rect> detectedObjects;
 
+        // Detection
         classifier.detectMultiScale(
                 frame,
                 detectedObjects, // Result of detection
@@ -61,16 +89,23 @@ int main(int argc, char **argv) {
                 frame.size()     // maximal size of the object
         );
 
-        cv::Mat detected(frame);
+        cv::Mat detected;
+        frame.copyTo(detected);
 
         for (const auto& rect: detectedObjects) {
             cv::rectangle(detected, rect, CV_RGB(255, 0, 0), 2);
         }
 
-        cv::imshow(cardet::videoOutputName, frame);
-        cv::imshow(cardet::detectedOutputName, detected);
+        cv::imshow(cardet::videoWindowName, frame);
+        cv::imshow(cardet::detectedWindowName, detected);
         cv::waitKey(20);
+
+        output << detected;
     }
+
+    // Release captures
+    output.release();
+    input.release();
 
     return 0;
 }
